eb18f2e8500e87362d247e4ed0f77fde0a39c18f
Anish Kumar Singh <v.anish.0014@gmail.com>
2022-04-09 18:05:07 +0530

    Attendance Management System

---
+++ about.php
@@ -0,0 +1,70 @@
+<?php
+  session_start();
+  $isIndex = 0;
+  if(!(array_key_exists('teacher_id',$_SESSION) && isset($_SESSION['teacher_id']))) {
+    session_destroy();
+    if(!$isIndex) header('Location: index.php');
+  }
+?>
+<?php include 'php/node_class.php'; ?>
+<html>
+ <head>
+  <link rel="stylesheet" href="css/style.css"/>
+  <title>About</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/highcharts.js"></script>
+  <script src="js/highcharts-exporting.js"></script>
+  <!-- Custom styles for this template -->
+    <link href="navbar-fixed-top.css" rel="stylesheet">
+ </head>
+ <body>
+   <!-- Fixed navbar -->
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <br>
+          <a class="navbar-brand" href="index.php" style="font-size:28px;">SRM Institute of Science and Technology</a><br />
+          <a class="navbar-brand" href="index.php">Online Attendance</a>
+        </div>
+
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li>
+			<a href="teacher.php">Dashboard</a></li>
+			<li><a href="profile.php">Profile</a></li>
+			<li><a href="student.php">Students</a></li>
+           	<li><a href="statistics.php">Statistics</a></li>
+			<li class="active"><a href="about.php">About</a></li>
+            	<li><a href="contact.php">Contact</a></li>
+			<li><a href="description.php">Description</a></li>
+			<li><a href="logout.php">Logout</a>
+		</li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav></br></br></br></br>
+  </div>
+<br>
+
+<h3><div>
+      <dl class="dl-horizontal">
+        <dd><h1 style="font-size: 50px;"><strong><br><b>SRM Institute of Science and Technology</b></strong></h1></dd>
+        <dd><h3>University Building,<br>SRM Nagar,<br>Kattankulathur,603203<br>Tamil nadu, INDIA</h3></dd>
+<br>
+
+<dd><h2><strong>Our Location:</strong></h2></dd>
+<dd>
+<iframe width="650" height="400" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3910.5901821120597!2d80.0422216664583!3d12.823038030748846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a52f712b82a78d9%3A0xfdb944a3aee53831!2sSRM%20Institute%20of%20Science%20and%20Technology!5e0!3m2!1sen!2sin!4v1649505835762!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</dd><br />
+</div>
+</h3>
+</html>
